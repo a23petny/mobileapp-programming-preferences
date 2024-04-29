0b1ec858bda7f559c18a31ba3a46c74a70b24d01
a23petny <a23petny@student.his.se>
2024-04-29 14:30:48 +0200

    ADDEDE the code that the course recomended i did

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    // in My MainActivity class
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +31,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                intent.putExtra("text", "Ronya Rövardotter:\nBlodet ska rinna!"); // Optional
-                intent.putExtra("number", 888); // Optional
+                //intent.putExtra("text", "Ronya Rövardotter:\nBlodet ska rinna!"); // Optional
+                //intent.putExtra("number", 888); // Optional
                 startActivity(intent);
             }
         });
+
+
+
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
 
     }
 
--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -2,13 +2,39 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void savePref(View view) {
+        // Get the text
+        EditText newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.settingseditview);
+
+        // Store the new preference
+        SharedPreferences.Editor myPreferenceEditor;
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        // Display the new preference
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+        // Clear the EditText
+        newPrefText.setText("");
+
     }
 }
