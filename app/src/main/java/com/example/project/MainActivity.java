b4029a628c3b2fb0053df19803554b36a3cc398b
a23petny <a23petny@student.his.se>
2024-04-29 11:52:38 +0200

    MADE button work that sends you to the othere activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("text", "Ronya Rövardotter:\nBlodet ska rinna!"); // Optional
+                intent.putExtra("number", 888); // Optional
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
